b67a19f6719702f15056d2d86eb31b3a603f62f0
Manoel Pedro <pedro.manoel@academico.ifpb.edu.br>
2023-06-02 20:03:43 -0300

    first commit

---
+++ src/app/shared/modelo/USUARIOS.ts
@@ -0,0 +1,39 @@
+import {Usuario} from './usuario';
+
+export const USUARIOS: Array<Usuario> = [
+  {
+    nome: 'Gustavo',
+    idade: 42,
+    cpf: '123',
+    id: 1,
+    telefone: '83999998888'
+  },
+  {
+    nome: 'Ana',
+    idade: 30,
+    cpf: '456',
+    id: 2,
+    telefone: '3399999999'
+  },
+  {
+    nome: 'Paulo',
+    idade: 25,
+    cpf: '789',
+    id: 3,
+    telefone: '08388888888'
+  },
+  {
+    nome: 'Carla',
+    idade: 35,
+    cpf: '012',
+    id: 4,
+    telefone: '99998888'
+  },
+  {
+    nome: 'Rafael',
+    idade: 45,
+    cpf: '345',
+    id: 5,
+    telefone: '1122334455'
+  }
+];
